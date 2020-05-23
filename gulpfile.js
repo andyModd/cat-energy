578225a20d107ed7f7796f0ce1dcfc8d5b710090
Andy <gl_andy@mail.ru>
2020-05-23 22:16:40 +0500

    Исправление критерия Б25

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var csso = require("gulp-csso");
 var server = require("browser-sync").create();
+var jsmin = require('gulp-jsmin');
 
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
@@ -26,7 +27,6 @@ gulp.task("clean", function () {
 gulp.task("copy", function() {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
     "source/js/**",
     "source/*.ico"
   ], {
@@ -44,7 +44,7 @@ gulp.task("posthtml", function() {
 });
 
 gulp.task("sprite", function() {
-  return gulp.src("source/img/icon-*.svg")
+  return gulp.src("source/img/icons/*.svg")
 
     .pipe(svgstore({
       inlineSvg: true
@@ -57,18 +57,18 @@ gulp.task("webp", function() {
   return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp({quality: 90}))
 
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("image", function() {
-  return gulp.src("source/img/**/*.{png,jpg,svg}")
+  return gulp.src("source/img/*.{png,jpg,svg}")
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
       imagemin.mozjpeg({progressive: true}),
       imagemin.svgo()
     ]))
 
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("css", function () {
@@ -79,6 +79,8 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+
+    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
@@ -105,5 +107,5 @@ gulp.task("refresh", function (done) {
   gulp.watch("source/*.html", gulp.series("posthtml", "refresh"));
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "posthtml"));
+gulp.task("build", gulp.series("clean", "copy", "image", "webp" "css", "sprite", "posthtml"));
 gulp.task("start", gulp.series("build", "server"));
